14662598c1d27e29b0b160b800048cf5336de494
Guilherme Jordão <62813340+jordaomoraes@users.noreply.github.com>
2022-10-25 10:25:15 -0300

    merge alteracoes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import GetIntegrations from "./scenarios/get-integracoes.js";
 import PostIntegracao1 from "./scenarios/post_integracao1.js";
 import PostIntegracao2 from "./scenarios/post_integracao2.js";
+import PostIntegracao3 from "./scenarios/post_integracao3";
+
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 import { group, sleep } from 'k6';
 
@@ -31,22 +33,33 @@ export const options = {
     },
     // post_integracao2: {
     //   executor: 'shared-iterations',
-    //   startTime: '20s',
-    //   vus: 5,
-    //   iterations: 20,
-    //   maxDuration: '15s'
+    //   startTime: '1s',
+    //   vus: 1,
+    //   iterations: 1,
+    //   maxDuration: '1s'
+    // },
+    // post_integracao3: {
+    //   executor: 'shared-iterations',
+    //   startTime: '1s',
+    //   vus: 1,
+    //   iterations: 1,
+    //   maxDuration: '1s'
     // },
   },
 };
 
 export default () => {
 
-  group('Endpoint Post Integrations 1 - hangfire - Integrador.Api', () => {
+  group('Endpoint Post Hangfire - Condicao Pagamento ', () => {
     PostIntegracao1();
   });
 
-  // group('Endpoint Post Integrations 2 - hangfire - Integrador.Api', () => {
+  // group('Endpoint Post Hangfire - Filiais ', () => {
   //   PostIntegracao2();
+  // });
+
+  // group('Endpoint Post Hangfire - Empresa ', () => {
+  //   PostIntegracao3();
   // });
 
   sleep(1);
---
+++ src/scenarios/post_integracao3.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+
+export default function () {
+  const url = 'http://10.10.3.2:5020/api/job';
+  const payload = JSON.stringify({
+    assemblyName: "Rech.Integracao.Empresa",
+    className: "EmpresaRunner",
+  });
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  http.post(url, payload, params);
+}
